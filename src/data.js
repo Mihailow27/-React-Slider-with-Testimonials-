47d544fc455005ba74fb55919802b02c81c64d1d
Dobromir Mihaylov <dobromirmihailow@icloud.com>
2023-07-25 23:02:29 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,35 @@
+const people = [
+  {
+    id: 1,
+    image: "https://www.course-api.com/images/people/person-1.jpeg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "A good manager is a one who isn't worried about his own career but rather the careers of those who work for him.",
+  },
+  {
+    id: 2,
+    image: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "john doe",
+    title: "regular guy",
+    quote:
+      "Success is not about being extraordinary; it's about being true to yourself and doing your best every day. Embrace your uniqueness, and you'll find greatness in being a regular guy.",
+  },
+  {
+    id: 3,
+    image: "https://www.course-api.com/images/people/person-3.jpeg",
+    name: "peter smith",
+    title: "product designer",
+    quote: "Design adds value faster than it adds costs.",
+  },
+  {
+    id: 4,
+    image: "https://www.course-api.com/images/people/person-2.jpeg",
+    name: "susan andersen",
+    title: "the boss",
+    quote:
+      "The best leaders are those most interested in surrounding themselves with assistants and associates smarter than they are.",
+  },
+];
+
+export default people;
